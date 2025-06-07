0182b0d2666b1dc783de23bc5ef7d1eee3de91ba
Michael Zewdie <michaelzewdie7@gmail.com>
2025-06-07 13:55:54 -0500

    fixed the file structure

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import '../styles/landing/landing.css'
 
-function App() {
+function LandingPage() {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default LandingPage
